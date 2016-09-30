1f78f0c5690e359bbfe683ed492d7a6e4aa89b24
Miles Florence <miles.b.flo@gmail.com>
2016-09-29 17:27:00 -0700

    refactored code to be more lua-y

--- jammer.lua
+++ jammer.lua
@@ -6,16 +6,15 @@ term.write("Scanning 1- for open frequencies...")
 
 sides = {'front','back','left','right','top','bottom'}
 
-function signalJam()
+function signalJam(m)
 	for i=1,65535 do
-		modem.open(i)
-		modem.transmit(i,i,"-Static-")
-		modem.close(i)
+		m.open(i)
+		m.transmit(i,i,"-Static-")
+		m.close(i)
 	end
 end
 
 for i=1,#sides do
-	term.write(sides[i])
 	if (peripheral.getType(sides[i]) == "modem" and peripheral.call(sides[i], 'isWireless')) then
 		modem = peripheral.wrap(sides[i])
 		break
@@ -24,10 +23,10 @@ end
 
 
 if (modem) then
--- 	while true do
--- 		signalJam()
--- 		sleep(10)
--- 	end
+	while true do
+		signalJam(modem)
+		sleep(10)
+	end
 else
 	term.write("No wireless modem found... Exiting.")
 end
