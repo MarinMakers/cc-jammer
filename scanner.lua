7d7adec54129b63375580f5954c5a5c87e03f575
Miles Florence <miles.b.flo@gmail.com>
2016-09-29 17:03:22 -0700

    Update scanner.lua
    scanner is now jammer

--- scanner.lua
+++ scanner.lua
@@ -1,3 +1,31 @@
+-- jammer
+
 term.clear()
-term.setCursorPos(1,1)
-term.write("Hello")
+term.setCursorPos(1,2)
+term.write("Scanning 1- for open frequencies...")
+
+sides = {'front','back','left','right','top','bottom'}
+
+for i=1,#sides do
+	if (peripheral.getType(sides[i]) == "modem" & peripheral.call(sides[i], 'isWireless')) do
+		modem = peripheral.wrap(sides[i])
+		break
+	end
+end
+
+function signalJam() do
+	for i=1,65535 do
+		modem.open(i)
+		modem.transmit(i,i,"-Static-")
+		modem.close(i)
+	end
+end
+
+if (modem) do
+	while true do
+		signalJam()
+		sleep(10)
+	end
+else do
+	term.write("No wireless modem found... Exiting.")
+end
